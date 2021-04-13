0e0c9ca65095412cc166d7e9997e78cf287ae7f3
Miłosz Buchacz <nerisox@gmail.com>
2021-04-13 16:01:58 +0200

    users data fetching

---
+++ src/components/users.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './users.css';
+
+class Users extends Component {
+    constructor() {
+        super()
+        this.state = {
+            users: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:8080/api/users')
+        .then(res => res.json())
+        .then(users => this.setState({users}, () => console.log('Users fetched.', users)));
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Users</h2>
+            </div>
+        )
+    }
+}
+
+export default Users;
