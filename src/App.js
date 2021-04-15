1eb1d305cad50adb78f6f8fc3eeefe4444e6baab
Miłosz Buchacz <nerisox@gmail.com>
2021-04-15 22:29:50 +0200

    axios on signup implemented, ready to refactor

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import Users from './components/users';
 import Login from './components/LoginScreen';
 
 function App() {
+
   return (
     <div className="App">
       <Login />
--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,23 +1,55 @@
 import React from 'react';
+// import './LoginStyle.css';
+import Axios from 'axios';
 
+class Login extends React.Component{
+  constructor(props){
+    super(props)
 
-export default function Login({ setToken }) {
-    return(
-      <div className="login-wrapper">
-        <h1>Please Log In</h1>
-        <form>
-          <label>
-            <p>Username</p>
-            <input type="text" />
-          </label>
-          <label>
-            <p>Password</p>
-            <input type="password" />
-          </label>
-          <div>
-            <button type="submit">Submit</button>
-          </div>
-        </form>
-      </div>
-    )
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+
+    this.state = {
+      username: '',
+      password: ''
+    };
+  
   }
+  
+  async onFormSubmit(){
+  try {
+    const response = await Axios.post('http://localhost:8080/api/auth/signup', {username: this.state.username, password: this.state.password });
+    console.log('Returned data:', response);
+  } catch (e) {
+    console.log(`Axios request failed: ${e}`);
+  }
+}
+
+
+
+
+
+  render(){
+      return(
+    <div className="login-wrapper">
+      <h1>Please Log In</h1>
+      <form onSubmit={this.onFormSubmit}>
+        <label>
+          <p>Username {this.state.username}</p>
+          <input type="text" value={this.state.username} onChange={(e) => this.setState({username: e.target.value})}/>
+        </label>
+        <label>
+          <p>Password {this.state.password}</p>
+          <input type="password" value={this.state.password} onChange={(e) => this.setState({password: e.target.value})}/>
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  )
+  }
+}
+
+
+
+export default Login;
