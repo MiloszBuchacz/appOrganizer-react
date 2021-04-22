71da7b16e1e73b812f840a8a93c06529cc80073a
Miłosz Buchacz <nerisox@gmail.com>
2021-04-23 00:51:11 +0200

    axios.get pulling

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -9,12 +9,13 @@ class UserPage extends Component{
         super(props)
 
         this.addNote = this.addNote.bind(this);
+        this.getNote = this.getNote.bind(this);
        // this.callBack = this.callBack.bind(this);
 
 
         this.state = {
             name: '',
-            body: ''
+            body: '',
         }
     }
     // callBack = (name) => {
@@ -26,14 +27,29 @@ class UserPage extends Component{
     async addNote(e) {
         e.preventDefault();
         try{
-        await axios.post('http://localhost:8080/api/notes', {name: this.state.name, body: this.state.body}, {headers: {'Authorization': `Bearer ${localStorage.getItem("token")}`}});
+        await axios.post('http://localhost:8080/api/notes',
+        {name: this.state.name, body: this.state.body},
+        {headers: {'Authorization': `Bearer ${localStorage.getItem("token")}`}});
+
         console.log("note Added!");
         } catch(err){
             console.log(err);
         }
         
     }
-    
+
+        async getNote(e) {
+            e.preventDefault();
+            try{
+            const { data } = await axios.get('http://localhost:8080/api/notes',
+            {headers: {'Authorization': `Bearer ${localStorage.getItem("token")}`}});
+            this.setState({name: data.name, body: data.body});
+            console.log(data);
+                console.log(this.state.name)  
+            }catch(err){
+                console.log(err);
+            }
+        }
 
     render(){
         return(
@@ -46,6 +62,10 @@ class UserPage extends Component{
                     <input className="body" type="text" value={this.state.body} placeholder="Enter your name" onChange={(e) => this.setState({body: e.target.value})} />
                     <button className="button" type="submit">zapisz</button>
                 </form>
+                <div>
+                    <button type="button" style={{marginTop: "160px", marginLeft: "200px"}} onClick={this.getNote}>siemanko</button>
+                    <p>{this.state.name, this.state.body}</p>
+                </div>
 
             </div> 
         )}
